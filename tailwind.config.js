0dcc88991025656b07091a05bb9a7dce400b7c71
mnyzio <nyziu84@gmail.com>
2023-06-07 18:10:31 -0400

    config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+};
+
 
